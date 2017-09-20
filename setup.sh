dff619974d73b3728c31fc294c73c15065945902
Stuart Campbell <stuart@harto.org>
2017-09-20 13:26:39 -0700

    helper fn in setup.sh

--- setup.sh
+++ setup.sh
@@ -18,8 +18,12 @@ brew bundle
 # Install configs
 #
 
-DOTFILES_DIR="$(cd $(dirname $0); pwd -P)/dotfiles"
-cd ~
-find $DOTFILES_DIR -type f | while read path; do
-  ln -fs $path
-done
+symlink_all() {
+  source=$1
+  target=$2
+  find $source -type f ! -name '*~' | while read path; do
+    ln -fs $path $target
+  done
+}
+
+symlink_all $HERE/dotfiles ~
