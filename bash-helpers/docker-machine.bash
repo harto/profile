627e3e24ccce0cf8aaedc14291fa88cd8104a4e9
Stuart Campbell <stuart@harto.org>
2020-06-12 11:28:40 -0700

    Remove unused bash helpers

--- bash-helpers/docker-machine.bash
+++
@@ -1,8 +0,0 @@
-dump-docker-machine-env() {
-  local machine="$1"
-  docker-machine env $machine > ~/.docker-machine-env.sh
-}
-
-if [[ -e ~/.docker-machine-env.sh ]]; then
-  source ~/.docker-machine-env.sh
-fi
